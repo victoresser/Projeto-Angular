54e9ce649c3e2c865456b614d6ded2d0eef97625
victoresser <victorribeiroesser@gmail.com>
2023-10-27 17:24:04 -0300

    Adicionando comportamento favoritos

--- memoteca/src/app/components/pensamento.service.ts
+++ memoteca/src/app/components/pensamento.service.ts
@@ -25,6 +25,22 @@ export class PensamentoService {
     return this.http.get<Pensamento[]>(this.API, { params });
   }
 
+  listarPensamentosFavoritos(
+    pagina: number,
+    filtro: string
+  ): Observable<Pensamento[]> {
+    const itensPorPagina = 6;
+
+    let params = new HttpParams()
+      .set('_page', pagina)
+      .set('_limit', itensPorPagina)
+      .set('favorito', true);
+
+    if (filtro.trim().length > 2) {
+      params = params.set('q', filtro);
+    }
+  }
+
   criar(pensamento: Pensamento): Observable<Pensamento> {
     return this.http.post<Pensamento>(this.API, pensamento);
   }
@@ -50,6 +66,6 @@ export class PensamentoService {
 
   mudarFavorito(pensamento: Pensamento): Observable<Pensamento> {
     pensamento.favorito = !pensamento.favorito;
-    return this.editar(pensamento)
+    return this.editar(pensamento);
   }
 }
