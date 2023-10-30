ad397f47ad67bda432eae92eea7c9356f9161ce0
victoresser <victorribeiroesser@gmail.com>
2023-10-30 08:44:10 -0300

    Projeto finalizado

--- memoteca/src/app/components/pensamento.service.ts
+++ memoteca/src/app/components/pensamento.service.ts
@@ -11,7 +11,11 @@ export class PensamentoService {
 
   constructor(private http: HttpClient) {}
 
-  listar(pagina: number, filtro: string): Observable<Pensamento[]> {
+  listar(
+    pagina: number,
+    filtro: string,
+    favoritos: boolean
+  ): Observable<Pensamento[]> {
     const itensPorPagina = 6;
 
     let params = new HttpParams()
@@ -22,23 +26,11 @@ export class PensamentoService {
       params = params.set('q', filtro);
     }
 
-    return this.http.get<Pensamento[]>(this.API, { params });
-  }
-
-  listarPensamentosFavoritos(
-    pagina: number,
-    filtro: string
-  ): Observable<Pensamento[]> {
-    const itensPorPagina = 6;
-
-    let params = new HttpParams()
-      .set('_page', pagina)
-      .set('_limit', itensPorPagina)
-      .set('favorito', true);
-
-    if (filtro.trim().length > 2) {
-      params = params.set('q', filtro);
+    if (favoritos) {
+      params = params.set('favorito', true);
     }
+
+    return this.http.get<Pensamento[]>(this.API, { params });
   }
 
   criar(pensamento: Pensamento): Observable<Pensamento> {
